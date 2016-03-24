b99a8a04943af23e77c482dbd684359564f099fd
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-03-24 15:40:17 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DeviantArt.Mixfile do
 
   def project do
     [app: :deviant_elixir,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
