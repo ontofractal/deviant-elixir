4de211e0ba11d8634b5e709b65cc03fe50217f59
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-03-24 15:39:56 +0200

    added example of DeviantArt.rss response schema

--- test/deviant_art_test.exs
+++ test/deviant_art_test.exs
@@ -4,7 +4,6 @@ defmodule DeviantArtTest do
 
   test "DA docs example rss query" do
     feed = DeviantArt.rss(%{query: "frogs", category: "digitalart/drawings", sort: "boost:popular+in"})
-    IO.inspect feed
     assert feed.title == "DeviantArt: Popular In:digitalart/drawings frogs"
   end
 end
